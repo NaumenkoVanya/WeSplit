818898bc368902443bb3fe7bd48f5112555208a2
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-09 15:34:49 +0300

    Update ContentView and begin 17 day work

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,48 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-      let students = ["Harry", "Hermiona", "Ron"]
-      @State private var selectedStudent = "Harry"
-    
-      @State private var tapCount = 0
-      @State private var name = ""
     
     var body: some View {
-
-        NavigationStack {
-            Form {
-                TextField("Enter your name", text: $name)
-                Text("Your name is \(name)")
-                
-                Section {
-                    Text("Hello, world!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        
-        NavigationStack {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-            }
-        }
-        
-        
-//        Form {
-//            ForEach(0..<100) {
-//                Text("Row \($0)")
-//            }
-//        }
-        
-        Button("Tap Count: \(tapCount)") {
-                    tapCount += 1
-                }
-        
+        Text("Hello World")
     }
 }
 
---
+++ WeSplit/WeSplit/WalkthroughByDay/SixteenDay.swift
@@ -0,0 +1,61 @@
+//
+//  16 day .swift
+//  WeSplit
+//
+//  Created by Ваня Науменко on 9.02.24.
+//
+
+import SwiftUI
+
+struct SixteenDay: View {
+    
+    let students = ["Harry", "Hermiona", "Ron"]
+    @State private var selectedStudent = "Harry"
+  
+    @State private var tapCount = 0
+    @State private var name = ""
+    
+    
+    var body: some View {
+        
+        NavigationStack {
+            Form {
+                TextField("Enter your name", text: $name)
+                Text("Your name is \(name)")
+                
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        
+        
+//        Form {
+//            ForEach(0..<100) {
+//                Text("Row \($0)")
+//            }
+//        }
+        
+        Button("Tap Count: \(tapCount)") {
+                    tapCount += 1
+                }
+        
+    }
+}
+
+
+#Preview {
+    SixteenDay()
+}
