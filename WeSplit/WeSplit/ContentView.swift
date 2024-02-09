401b023483fc55eeae894285ab5ea7cbfc4417b6
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-09 15:02:58 +0300

    add @state and $

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,17 +10,19 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
         
         Button("Tap Count: \(tapCount)") {
                     tapCount += 1
                 }
-        
 
-        
         NavigationStack {
             Form {
+                TextField("Enter your name", text: $name)
+                Text("Your name is \(name)")
+                
                 Section {
                     Text("Hello, world!")
                 }
