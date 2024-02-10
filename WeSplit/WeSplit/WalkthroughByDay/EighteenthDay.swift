3d8b30eb65d98e1309d3af266890ca28f98a08e3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-11 00:23:05 +0300

    Add 18 Day
    Add foto testa 18 day work

---
+++ WeSplit/WeSplit/WalkthroughByDay/EighteenthDay.swift
@@ -0,0 +1,20 @@
+//
+//  EighteenthDay.swift
+//  WeSplit
+//
+//  Created by Ваня Науменко on 11.02.24.
+//
+
+import SwiftUI
+
+struct EighteenthDay: View {
+    var body: some View {
+        Image ("ImageTest18Day")
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    EighteenthDay()
+}
